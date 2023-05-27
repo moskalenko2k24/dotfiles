74b76b624f10fed0382480b529bca1c228c4f3d5
moskalenco_a <moskalenco.a@gmail.com>
2023-05-28 02:00:56 +0300

    Add workaround for Error E5248, fix formatting

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -5,38 +5,42 @@ require('mason-lspconfig').setup {
 }
 
 -- LSP
-local custom_attach = function(client, bufnr, elses)
-    local opts = { buffer = bufnr, noremap = true, silent = true }
-    local function buf_set_keymap(mode, lhs, rhs)
-        vim.keymap.set(mode, lhs, rhs, opts)
-    end
-	print('LSP started');
-    -- require('completion').on_attach(client)
-    -- require('diagnostic').on_attach(client)
-    if client.supports_method('textDocument/hover') then
-        buf_set_keymap('n', 'K', vim.lsp.buf.hover);
-    end
-    if client.supports_method('textDocument/declaration') then
-        buf_set_keymap('n', 'gD', vim.lsp.buf.declaration);
-    end
-    buf_set_keymap('n', 'gd', vim.lsp.buf.definition);
-    buf_set_keymap('n', 'gr', vim.lsp.buf.references);
-    buf_set_keymap('n', '<Leader>rn', vim.lsp.buf.rename);
-    buf_set_keymap('n', '[d', vim.diagnostic.goto_prev);
-    buf_set_keymap('n', ']d', vim.diagnostic.goto_next);
+local custom_attach = function(client, bufnr)
+  -- Workaround for https://github.com/OmniSharp/omnisharp-roslyn/issues/2531
+  if client.name == "omnisharp" then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  local function buf_set_keymap(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+  print('LSP started');
+  -- require('completion').on_attach(client)
+  -- require('diagnostic').on_attach(client)
+  if client.supports_method('textDocument/hover') then
+    buf_set_keymap('n', 'K', vim.lsp.buf.hover);
+  end
+  if client.supports_method('textDocument/declaration') then
+    buf_set_keymap('n', 'gD', vim.lsp.buf.declaration);
+  end
+  buf_set_keymap('n', 'gd', vim.lsp.buf.definition);
+  buf_set_keymap('n', 'gr', vim.lsp.buf.references);
+  buf_set_keymap('n', '<Leader>rn', vim.lsp.buf.rename);
+  buf_set_keymap('n', '[d', vim.diagnostic.goto_prev);
+  buf_set_keymap('n', ']d', vim.diagnostic.goto_next);
 end
 
 local setupObj = {
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    on_attach = custom_attach,
-    settings = {
-      Lua = {
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  on_attach = custom_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
       },
     },
+  },
 }
 
 -- After setting up mason-lspconfig you may set up servers via lspconfig
