6ad4f30dd2e33ce2d2d98aa607846ca8483ad7d7
moskalenco_a <moskalenco.a@gmail.com>
2024-06-26 14:04:33 +0300

    Add plugin that shows startup time

--- dotconfig/nvim/lua/plugins/packer.lua
+++ dotconfig/nvim/lua/plugins/packer.lua
@@ -165,6 +165,8 @@ return require('packer').startup(function(use)
 
   use 'Abstract-IDE/abstract-autocmds';
 
+  use 'dstein64/vim-startuptime';
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
