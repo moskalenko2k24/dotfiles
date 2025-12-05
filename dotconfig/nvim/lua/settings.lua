356dbe5a0e3a2279d215e7c35cd79e31c00e9afe
moskalenco_a <moskalenco.a@gmail.com>
2025-12-05 14:30:45 +0200

    Add smth for cyrillic mappings

--- dotconfig/nvim/lua/settings.lua
+++ dotconfig/nvim/lua/settings.lua
@@ -4,6 +4,29 @@ local utils = require('utils')
 local set_indent = utils.set_indent;
 local set_indent_for = utils.set_indent_for;
 
+--- https://github.com/Wansmer/langmapper.nvim
+--- "Show example of `vim.opt.langmap`"
+
+local function escape(str)
+  -- You need to escape these characters to work correctly
+  local escape_chars = [[;,."|\]]
+  return vim.fn.escape(str, escape_chars)
+end
+
+-- Recommended to use lua template string
+local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+
+vim.opt.langmap = vim.fn.join({
+    -- | `to` should be first     | `from` should be second
+    escape(ru_shift) .. ';' .. escape(en_shift),
+    escape(ru) .. ';' .. escape(en),
+}, ',')
+
+---
+
 -- MAIN SETTINGS
 vim.cmd 'syntax on'                  -- syntax highlighting
 opt.compatible = false               -- no compatible with Vi
